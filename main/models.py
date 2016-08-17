09440ea4e1b283b19f43b965eacf7f1a8489b111
haoguangbo <haoguangbo@yeah.net>
2016-08-17 16:38:29 +0800

    models初步制定

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='person')
+    name = models.CharField(max_length=50, default='新用户')
+    avatar=models.FileField(upload_to='avatars')
+    introduction= models.CharField(max_length=100, default='暂无个人介绍')
+    phone = models.CharField(max_length=20, blank=True, default='')
+    last_modified_time = models.DateTimeField(auto_now=True)
+
+
+class Story(models.Model):
+    first_word=models.CharField(max_length=500, default='')
+    create_user=models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='stories_created')
+    words=models.ManyToManyField('Word',related_name='story')
+
+
+class Word(models.Model):
+    text=models.CharField(max_length=50, default='')
+    user=models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='stories_participated')
