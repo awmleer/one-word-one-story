716dbb77b61160bc4c73ffc7a8473b74ff74e4cc
haoguangbo <haoguangbo@yeah.net>
2016-08-17 17:13:36 +0800

    register models in admin site

--- main/models.py
+++ main/models.py
@@ -7,14 +7,19 @@ class Person(models.Model):
     introduction= models.CharField(max_length=100, default='暂无个人介绍')
     phone = models.CharField(max_length=20, blank=True, default='')
     last_modified_time = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
 
 
 class Story(models.Model):
     first_word=models.CharField(max_length=500, default='')
     create_user=models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='stories_created')
     words=models.ManyToManyField('Word',related_name='story')
-
+    def __str__(self):
+        return self.first_word
 
 class Word(models.Model):
     text=models.CharField(max_length=50, default='')
     user=models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='stories_participated')
+    def __str__(self):
+        return self.text
