093a4ba7b6ba675416f50cf21f35498d89f61f1e
haoguangbo <haoguangbo@yeah.net>
2016-08-17 16:39:58 +0800

    static 和 media 的配置

--- OneWordOneStory/settings.py
+++ OneWordOneStory/settings.py
@@ -120,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/var/www/story/static'
+if DEBUG:
+    STATIC_ROOT=os.path.join(BASE_DIR,'static')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static/'),
+    )
+
+
+MEDIA_ROOT='/var/www/story/media'
+if DEBUG:
+    MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
