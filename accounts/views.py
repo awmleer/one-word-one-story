e686752520574130de5a42aad98045dd8c8c2fd4
haoguangbo <haoguangbo@yeah.net>
2016-08-18 11:57:57 +0800

    login

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
-from django.contrib.auth import authenticate #用户登录认证
+import django.contrib.auth as auth #用户登录认证
 from django.contrib.auth.decorators import login_required,permission_required
 from main.models import *
 import datetime
@@ -24,5 +24,18 @@ def login(request):
         res=render(request, 'login.html', context)
     # 如果是POST请求
     else:
-        res=HttpResponse('success', content_type="text/plain")
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                # User is valid, active and authenticated
+                auth.login(request, user)
+                res = HttpResponse('success', content_type="text/plain")
+            else:
+                # The password is valid, but the account has been disabled!
+                res = HttpResponse('您的账号已被锁定', content_type="text/plain")
+        else:
+            # the authentication system was unable to verify the username and password
+            # The username and password were incorrect.
+            res = HttpResponse('用户名或密码错误', content_type="text/plain")
     return res
