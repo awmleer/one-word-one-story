309f9023fecad5eb54e276f86278cf3d6584fe1f
haoguangbo <haoguangbo@yeah.net>
2016-08-18 22:54:07 +0800

    signup page

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,6 @@ logger = logging.getLogger('django')
 
 @require_http_methods(["GET","POST"])
 def login(request):
-    logger.info(request.method)
     # 如果是GET请求
     if request.method=='GET':
         context={}
@@ -39,6 +38,33 @@ def login(request):
             # The username and password were incorrect.
             res = HttpResponse('用户名或密码错误', content_type="text/plain")
     return res
+
+
+
+@require_http_methods(["GET","POST"])
+def signup(request):
+    # 如果是GET请求
+    if request.method=='GET':
+        context={}
+        res=render(request, 'signup.html', context)
+    # 如果是POST请求
+    else:
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                # User is valid, active and authenticated
+                auth.login(request, user)
+                res = HttpResponse('success', content_type="text/plain")
+            else:
+                # The password is valid, but the account has been disabled!
+                res = HttpResponse('您的账号已被锁定', content_type="text/plain")
+        else:
+            # the authentication system was unable to verify the username and password
+            # The username and password were incorrect.
+            res = HttpResponse('用户名或密码错误', content_type="text/plain")
+    return res
+
 
 
 def logout(request):
